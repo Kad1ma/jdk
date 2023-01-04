a3793a9e9e81b2afc1c96a50389323afaebdc6e7
lvxiaosheng <lvxiaosheng@bestpay.com.cn>
2023-01-04 14:20:45 +0800

    init

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        Map<String, String> map = new HashMap<>();
+        map.put("123", "123");
+    }
+}
